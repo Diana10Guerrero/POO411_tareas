f34655f1b32d674591e91b97ee44770f4586a602
diana guerrero <dianaguerrero191997@gmail.com>
2026-07-12 15:52:12 -0400

    cambio el tipo de puntero, (casi como volver a empezar)

--- ejercicios/Materia.cpp
+++ ejercicios/Materia.cpp
@@ -1,19 +1,15 @@
-//incluyo plantilla de materia
+// Incluyo plantilla de materia
 #include "Materia.h"
 #include <iostream>
 #include <fstream>
-#include <string>
 
-//defino constructor de materia
-Materia::Materia(std::string _nombre, float _codigo, float _creditos) {
-    this->nombre = _nombre;
-    this->codigo = _codigo;
-    this->creditos = _creditos;
-}
-// Método para mostrar información de la materia
-void Materia::mostrar_info() {
-    std::cout << "Materia: " << nombre
-              << ", Código: " << codigo
-              << ", Créditos: " << creditos << std::endl;
-}
+// Defino constructor de Materia
+Materia::Materia(std::string _nombre, float _codigo, float _creditos)
+{
+    std::string *pNombre = &_nombre;
+    float *pCodigo = &_codigo;
+    float *pCreditos = &_creditos;
 
+    nombre = *pNombre;
+    codigo = *pCodigo;
+    creditos = *pCreditos;
